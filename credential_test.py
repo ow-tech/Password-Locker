5c7e1152d87c31df3f1bf8a73d92f22d06b71239
ow-tech <wagwanwekon@outloo.com>
2020-04-20 14:15:43 +0300

    test for saving new credential

--- credential_test.py
+++ credential_test.py
@@ -22,5 +22,13 @@ class TestCredential(unittest.TestCase):
             self.assertEqual(self.new_credential.credential_name,"Instagram")
             self.assertEqual(self.new_credential.usr_name,"Alex123")
             self.assertEqual(self.new_credential.password,"123alex")
+        
+        def test_save_credential(self):
+            '''
+            test case to ensure user saves his/her logins
+
+            '''
+            self.new_credential.save_credential()
+            self.assertEqual(len(Credential.credential_list),1)
 if __name__ == '__main__':
     unittest.main()
