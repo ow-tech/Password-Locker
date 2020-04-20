67d3762978544b860f642f42fb26186692cc28ac
ow-tech <wagwanwekon@outloo.com>
2020-04-20 15:33:41 +0300

    creation of save user function that is responsible for saving  new user instances

--- run.py
+++ run.py
@@ -4,5 +4,14 @@ from credential import Credential
 
 
 def create_user(user_name,user_login):
+    '''
+    Function to create new user
+    '''
     new_user = User(user_name,user_login)
     return new_user
+
+def save_user(user):
+    '''
+    Function to saver new user
+    '''
+    user.save_user():
