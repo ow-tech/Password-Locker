bc757ab15591551dcdba8df5243dbeee2ee367eb
ow-tech <wagwanwekon@outloo.com>
2020-04-20 13:48:17 +0300

    test for saving user

--- user.py
+++ user.py
@@ -2,6 +2,10 @@ class User:
     ''''
     Class that authenticates User
     '''
+
+    user_list= []
     def __init__(self,user_name,user_login):
         self.user_name = user_name
         self.user_login = user_login
+    
+    def save_user(self):
--- user_test.py
+++ user_test.py
@@ -19,3 +19,10 @@ class TestUser(unittest.TestCase):
     def test_init(self):
         self.assertEqual(self.new_user.user_name,"Alex123")
         self.assertEqual(self.new_user.user_login,"123alex")
+
+    def test_save_user(self):
+        '''
+        test to ensure that users are being saved as they are created
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list)1)
