4a02209c1372bdeadcb1b0abcaf31c3a38191dc2
ow-tech <wagwanwekon@outloo.com>
2020-04-20 15:31:29 +0300

    creation of create user function that is responsible for creating new user instances

--- run.py
+++ run.py
@@ -1,3 +1,8 @@
 #!/urs/bin/env python3.6
 from user import User
 from credential import Credential
+
+
+def create_user(user_name,user_login):
+    new_user = User(user_name,user_login)
+    return new_user
