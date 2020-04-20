1b9bf2b973d623f33aa8feb8ff293aacc7263d16
ow-tech <wagwanwekon@outloo.com>
2020-04-20 13:38:07 +0300

    creation of user class and initialization

---
+++ user.py
@@ -0,0 +1,7 @@
+class User:
+    ''''
+    Class that authenticates User
+    '''
+    def __init__(self,user_name,user_login):
+        self.user_name = user_name
+        self.user_login = user_login
