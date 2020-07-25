02330e2d0b86a936c98dd78a0cbdde3bdbd26af7
ow-tech <wagwanwekon@outloo.com>
2020-07-25 22:01:07 +0300

    created class credential and its methods with corresponding tests for each method behaviour

--- credential.py
+++ credential.py
@@ -1,51 +1,76 @@
-import random
+import random 
 import string
 import pyperclip
+
+# class Credential:
+
+#     credential_list = []
+#     '''
+#     class that generates new instances of credentials
+#     '''
+#     def __init__(self,credential_name, user_name,password):
+#         self.credential_name = credential_name
+#         self.user_name = user_name
+#         self.password = password
+
+#     def save_credential(self):
+#         '''
+#         method that saves new credential
+#         '''
+#         Credential.credential_list.append(self)
+
+#     def delete_credential(self):
+#         '''
+#         method that deletes credential
+#         '''
+#         Credential.credential_list.remove(self)
+    
+
+#     @classmethod
+#     def find_by_name(cls,name):
+#         for credential in cls.credential_list:
+#             if credential.password == name:
+#                 return credential
+
+#         return False
+
+#     @classmethod
+#     def display_credential(cls):
+#         '''
+#         method that returns all of the credentials in credential list
+#         '''
+#         return cls.credential_list 
+
+#     def genarate_password(stringLength=8):
+#         '''
+#         method for randomly generating a password
+#         '''
+#         generatedPassword = string.asscii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
+#         return ''.join(random.choice(generatedPassword)for i in range(stringLength))
+
 
 class Credential:
 
     credential_list = []
-    '''
-    class that generates new instances of credentials
-    '''
-    def __init__(self,credential_name, user_name,password):
-        self.credential_name = credential_name
-        self.user_name = user_name
-        self.password = password
 
+    def __init__(self, account_name, account_user_name, account_password):
+        self.account_name = account_name
+        self.account_user_name = account_user_name
+        self.account_password = account_password
     def save_credential(self):
-        '''
-        method that saves new credential
-        '''
         Credential.credential_list.append(self)
-
-    def delete_credential(self):
-        '''
-        method that deletes credential
-        '''
-        Credential.credential_list.remove(self)
-    
-
     @classmethod
-    def find_by_name(cls,name):
+    def find_by_name(cls, name):
         for credential in cls.credential_list:
-            if credential.password == name:
+            if credential.account_name == name:
                 return credential
-
         return False
-
+    def delete_credential(self):
+        Credential.credential_list.remove(self)
     @classmethod
-    def display_credential(cls):
-        '''
-        method that returns all of the credentials in credential list
-        '''
-        return cls.credential_list 
-
-    def genarate_password(stringLength=8):
-        '''
-        method for randomly generating a password
-        '''
-        generatedPassword = string.asscii_uppercase + string.ascii_lowercase + string.digits + "~!@#$%^&*"
-        return ''.join(random.choice(generatedPassword)for i in range(stringLength))
-
-
+    def display_all_credentials(cls):
+        return cls.credential_list
+    @classmethod
+    def copy_password(cls,name):
+        credential_found = Credential.find_by_name(name)
+        pyperclip.copy(credential_found.account_password)
--- credential_test.py
+++ credential_test.py
@@ -1,75 +1,111 @@
 import unittest
 from credential import Credential
+import pyperclip
+
+# class TestCredential(unittest.TestCase):
+#         '''
+#     Test class to define behavious of Credential class
+
+#     Args:
+#         unittest.TestCase: TestCase class that helps creating test case:
+#     '''
+
+#         def setUp(self):
+#             '''
+#             Set up method to run before each test cases.
+#             '''
+#             self.new_credential= Credential("Instagram","Alex123","123alex")
+
+#         def test_init(self):
+#             '''
+#             test case to test if the object is initialized properly
+#             '''
+#             self.assertEqual(self.new_credential.credential_name,"Instagram")
+#             self.assertEqual(self.new_credential.usr_name,"Alex123")
+#             self.assertEqual(self.new_credential.password,"123alex")
+        
+#         def test_save_credential(self):
+#             '''
+#             test case to ensure user saves his/her logins
+
+#             '''
+#             self.new_credential.save_credential()
+#             self.assertEqual(len(Credential.credential_list),1)
+
+
+#         def tearDown(self):
+            
+#             Credential.credential_list = []    
+
+#         def test_save_multiple_credentials(self):
+#                 '''
+#                 test that checks if multiple user objects are saved
+#                 '''
+#                 self.new_credential.save_credential()
+#                 test_credential=Credential("Intagram","Kim123","123kim") #new Credential
+#                 test_credential.save_credential()
+#                 self.assertEqual(len(Credential.credential_list),2)
+
+#         def test_delete_credential(self):
+
+#             self.new_credential.save_credential()
+#             test_credential = Credential("Intagram","Kim123","123kim")
+#             test_credential.save_credential()
+
+
+#             self.new_credential.delete_credential()
+#             self.assertEqual(len(Credential.credential_list),1)
+
+#         def test_find_credential_by_credential_name(self):
+
+#             self.new_credential.save_credential()
+#             test_credential = Credential("Intagram","Kim123","123kim")
+#             test_credential.save_credential()
+
+#             found_credential = Credential.find_by_name("123kim")
+#             self.assertEqual(found_credential.password,test_credential.password)
+
+#         def test_display_all_saved_credentials(self):
+#             '''
+#             test for displaying saved credential
+#             '''
+#             self.assertEqual(Credential.display_credential(),Credential.credential_list)
+
+
 
 class TestCredential(unittest.TestCase):
-        '''
-    Test class to define behavious of Credential class
-
-    Args:
-        unittest.TestCase: TestCase class that helps creating test case:
-    '''
-
-        def setUp(self):
-            '''
-            Set up method to run before each test cases.
-            '''
-            self.new_credential= Credential("Instagram","Alex123","123alex")
-
-        def test_init(self):
-            '''
-            test case to test if the object is initialized properly
-            '''
-            self.assertEqual(self.new_credential.credential_name,"Instagram")
-            self.assertEqual(self.new_credential.usr_name,"Alex123")
-            self.assertEqual(self.new_credential.password,"123alex")
-        
-        def test_save_credential(self):
-            '''
-            test case to ensure user saves his/her logins
-
-            '''
-            self.new_credential.save_credential()
-            self.assertEqual(len(Credential.credential_list),1)
-
-
-        def tearDown(self):
-            
-            Credential.credential_list = []    
-
-        def test_save_multiple_credentials(self):
-                '''
-                test that checks if multiple user objects are saved
-                '''
-                self.new_credential.save_credential()
-                test_credential=Credential("Intagram","Kim123","123kim") #new Credential
-                test_credential.save_credential()
-                self.assertEqual(len(Credential.credential_list),2)
-
-        def test_delete_credential(self):
-
-            self.new_credential.save_credential()
-            test_credential = Credential("Intagram","Kim123","123kim")
-            test_credential.save_credential()
+    def setUp(self):
+        self.new_credential = Credential('Instagram','alexinsta','alex1234565')
+    def tearDown(sel):
+        Credential.credential_list = []
+    def test_init(self):
+        self.assertTrue(self.new_credential.account_name, 'Instagram')
+    def test_save_credential(self):
+        self.new_credential.save_credential()
+        self.assertTrue(len(Credential.credential_list), 1)
+    def test_save_multiple_credentials(self):
+        self.new_credential.save_credential()
+        self.another_credential = Credential('Twitter', 'TwitterAlex','Twitterpassword')
+        self.another_credential.save_credential()
+        self.assertTrue(len(Credential.credential_list),2)
+    def test_credential_exists(self):
+        self.new_credential.save_credential()
+        test_credential = Credential('Twitter', 'TwitterAlex','Twitterpassword')
+        test_credential.save_credential()
+        credential_exists = Credential.find_by_name('Twitter')
+        self.assertTrue(credential_exists)
+    def test_delete_credential(self):
+        self. new_credential.save_credential()
+        self.new_credential.delete_credential()
+        self.assertEqual(len(Credential.credential_list),0)
+    def test_display_all_credentials(self):
+        self.assertEqual (Credential.display_all_credentials(),Credential.credential_list)
+    def test_copy_credential_password(self):
+        self.new_credential.save_credential()
+        Credential.copy_password('Instagram')
+        self.assertTrue(self.new_credential.account_password, pyperclip.paste())
 
 
-            self.new_credential.delete_credential()
-            self.assertEqual(len(Credential.credential_list),1)
 
-        def test_find_credential_by_credential_name(self):
-
-            self.new_credential.save_credential()
-            test_credential = Credential("Intagram","Kim123","123kim")
-            test_credential.save_credential()
-
-            found_credential = Credential.find_by_name("123kim")
-            self.assertEqual(found_credential.password,test_credential.password)
-
-        def test_display_all_saved_credentials(self):
-            '''
-            test for displaying saved credential
-            '''
-            self.assertEqual(Credential.display_credential(),Credential.credential_list)
-
-            
 if __name__ == '__main__':
     unittest.main()
